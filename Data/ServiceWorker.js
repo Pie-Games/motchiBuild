4758f72d46dc22c7824900a2c5bee4c577b97b4f
Baste <assasinadyg@gmail.com>
2025-05-27 14:11:16 +0300

    CChange jslib pay stars

--- Data/ServiceWorker.js
+++ Data/ServiceWorker.js
@@ -1,7 +1,7 @@
 const cacheName = "DefaultCompany-motchi-1.0";
 const contentToCache = [
     "Build/b7c832fa38a2969eee8c250865f28a3c.loader.js",
-    "Build/0a74755f3bdf7ed356dbeacd38f15fc4.framework.js.br",
+    "Build/b8085d4c6316e23bbf40150d884ef5f1.framework.js.br",
     "Build/4488f0381969c37ec3371e9a69eacdfc.data.br",
     "Build/12ff6236c80aafd60547c17c4b21b34d.wasm.br",
     "TemplateData/style.css"
