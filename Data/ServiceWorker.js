f7b4d350c0a91f893154e1817b035c6b47c226a1
Baste <assasinadyg@gmail.com>
2025-05-30 11:45:24 +0300

    Transaction build

--- Data/ServiceWorker.js
+++ Data/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-motchi-1.0";
 const contentToCache = [
-    "Build/b95a8c890cf7f0143bd521e7877fcd43.loader.js",
-    "Build/4bfc0de2067aa4649a61e1a7fefa4709.framework.js.br",
-    "Build/961fd2c241958e819393354dab4c9460.data.br",
-    "Build/878c3c395c3f9980320ff90930250a1f.wasm.br",
+    "Build/a2ef6bc4e9ca800bd798995cef3cbfdb.loader.js",
+    "Build/b8104040d19209442e885f2d0ee3d67b.framework.js.br",
+    "Build/878947d881da46a3498c0533f39d150c.data.br",
+    "Build/0651bb41bc5f0595c08f5bbf0a849440.wasm.br",
     "TemplateData/style.css"
 
 ];
