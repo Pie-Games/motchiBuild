60ae001176c5f6850ee56e799a459db48a756f8b
Baste <assasinadyg@gmail.com>
2025-07-03 12:23:45 +0300

    skin tutor, bugfixes

--- Data/ServiceWorker.js
+++ Data/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-motchi-1.0";
 const contentToCache = [
-    "Build/ccf0d5a13b97094e18db963ec391827d.loader.js",
-    "Build/920a2d517d5688a5b3fa0ec78b72ac93.framework.js.br",
-    "Build/2925150868b5ba038749af3b8765b4f4.data.br",
-    "Build/1cb36b6abce32dc595c089766b2839c5.wasm.br",
+    "Build/df044548b40ab41a9741002f8a107a2f.loader.js",
+    "Build/9289f84a8a5d00e93aa9a66a1d3022f1.framework.js.br",
+    "Build/27eeffd830f6b8d43df920d8e59c471d.data.br",
+    "Build/1a4289ddfd061a9145088df3103025bd.wasm.br",
     "TemplateData/style.css"
 
 ];
